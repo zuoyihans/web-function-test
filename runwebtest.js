b97f321423a70e5b858a7e91b0fd7851306ebed0
HuiDong Wu <wuhd@cn.ibm.com>
2019-04-15 17:13:38 +0800

    edit

--- runwebtest.js
+++ runwebtest.js
@@ -1,5 +1,33 @@
 #!/usr/bin/env node
+const program = require('commander');
+const chalk = require('chalk');
+const pkg = require('./package.json');
+
+const { log } = console;
 const { main } = require('./index');
+
+program.version(pkg.version)
+  .option('-v --version', 'print version')
+  .option('--config [path_to_config]', 'set config file path');
+
+function displayUsage() {
+  log(chalk.green('for help , just type '));
+  log(chalk.green('runwebtest -h '));
+  log('');
+  log(chalk.red('* config is mandontory'));
+  log(chalk.green('sample: updateExecution --config=./config.json'));
+  process.exit(1);
+}
+
+
+program.on('--help', displayUsage);
+
+program.parse(process.argv);
+
+
+if (!program.config) {
+  displayUsage();
+}
 
 (async () => {
   await main(process.argv[2]);
--- updateExecution.js
+++ updateExecution.js
@@ -1,6 +1,35 @@
 #!/usr/bin/env node
+const program = require('commander');
+const chalk = require('chalk');
+const pkg = require('./package.json');
+
+const { log } = console;
 const { updateExecution } = require('./index');
 
+
+program.version(pkg.version)
+  .option('-v --version', 'print version')
+  .option('--config [path_to_config]', 'set config file path');
+
+function displayUsage() {
+  log(chalk.green('for help , just type '));
+  log(chalk.green('updateExecution -h '));
+  log('');
+  log(chalk.red('* config is mandontory'));
+  log(chalk.green('sample: updateExecution --config=./config.json'));
+  process.exit(1);
+}
+
+
+program.on('--help', displayUsage);
+
+program.parse(process.argv);
+
+
+if (!program.config) {
+  displayUsage();
+}
+
 (async () => {
-  await updateExecution(process.argv[2]);
+  await updateExecution(program.config);
 })();
